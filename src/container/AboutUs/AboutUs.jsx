8fc0c17668694acea7a045a83758213dc15b3bb7
Cecep Abdul Rahman <cecepar94@gmail.com>
2022-04-01 02:01:40 +0700

    Add about

---
+++ src/container/AboutUs/AboutUs.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import './AboutUs.css';
+
+const AboutUs = () => (
+  <div className="app__aboutus flex__center section__padding" id="about">
+    <div className="app__aboutus-content flex__center">
+    <div className=".app__aboutus-content">
+    <ul className="app__wrapper-links">
+        <li className="p__opensans"><a href="#todays">Today's Offer</a></li>
+        <li className="p__opensans"><a href="#waiki">WAIKI Rice Box</a></li>
+        <li className="p__opensans"><a href="#aigo">Aigo</a></li>
+        <li className="p__opensans"><a href="#chingu">Chingu</a></li>
+        <li className="p__opensans"><a href="#seerameon">Seerameon Ikan</a></li>
+        <li className="p__opensans"><a href="#samjang">Samjang Gami (Extra Pedas)</a></li>
+        <li className="p__opensans"><a href="#omo">Omo</a></li>
+        <li className="p__opensans"><a href="#saranghaeyo">Saranghaeyo</a></li>
+      </ul>
+      </div>
+    </div>
+  </div>
+);
+
+export default AboutUs;
