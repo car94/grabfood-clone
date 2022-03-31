6a2ce062143d5e9c66b7438bba68d7f736b93ae3
Cecep Abdul Rahman <cecepar94@gmail.com>
2022-04-01 02:04:44 +0700

    Update heading

---
+++ src/components/SubHeading/SubHeading.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SubHeading = ({title}) => (
+  <div style={{ marginBottom: '1rem' }}>
+    <p className="p__opensans">{title}</p>
+  </div>
+);
+
+export default SubHeading;
