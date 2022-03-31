513ebb38b6b52fe1d672891740491f6391aa6103
Cecep Abdul Rahman <cecepar94@gmail.com>
2022-04-01 02:01:15 +0700

    Add static data

---
+++ src/constants/data.js
@@ -0,0 +1,15 @@
+import images from './images';
+
+const todays = [
+  {
+    title: 'Sambal Gami Cumi + free nasi',
+    imgUrl: images.todays,
+    tags: 'Sambal gami cumi dimasak diatas cobek tanah liat',
+  },
+];
+
+
+
+
+
+export default { todays};
---
+++ src/constants/images.js
@@ -0,0 +1,8 @@
+
+import grabfood from '../assets/grabfood.png';
+import todays from '../assets/todays.jpeg';
+
+export default {
+  grabfood,
+  todays,
+};
