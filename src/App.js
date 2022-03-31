22e09bce7bbbc302d25b5be59dc487cb53180b2b
Cecep Abdul Rahman <cecepar94@gmail.com>
2022-04-01 01:59:57 +0700

    Update css

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import React from 'react';
 
-import { AboutUs, Chef, FindUs, Footer, Gallery, Header, SpecialMenu } from './container';
+import { AboutUs, Footer, Header, SpecialMenu } from './container';
 import { Navbar } from './components';
 import './App.css';
 
 const App = () => (
   <div>
     <Navbar />
-    {/*<Header />
+    <Header />
     <AboutUs />
     <SpecialMenu />
-    <Chef />
-    <Gallery />
-    <FindUs />
-    <Footer /> */}
+    <Footer />
   </div>
 );
 
